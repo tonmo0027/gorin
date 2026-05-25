45e58ba751e98228ef582d08c191e33c06dce4f0
unknown <A320@dn1-001.local>
2026-05-25 17:54:11 +0900

    面白いよ

--- 遊び/main.js
+++ 遊び/main.js
@@ -1,20 +1,29 @@
 "use strict"
-const canvas = document.querySelector("#canvas")
-const ctx = canvas.getContext("2d")
-const W = canvas.clientWidth
-const H = canvas.clientHeight
-const size_triangle = 35
-console.log(ctx)
+const canvases = document.querySelectorAll("#canvas")
+const ctxes = []
+canvases.forEach(canvas => {
+    ctxes.push(canvas.getContext("2d"))
+});
+
+console.log(canvases)
+console.log(ctxes)
+const W = canvases[0].clientWidth
+const H = canvases[0].clientHeight
+const size_triangle = 60
 
 window.onload = function init() {
-    ctx.beginPath()
-    ctx.lineWidth = 1
-    ctx.strokeStyle = "rgb(60, 255, 0)"
-    ctx.moveTo(W/2, H/2-size_triangle)
-    ctx.lineTo(W/2-size_triangle, H/2+size_triangle)
-    ctx.lineTo(W/2+size_triangle, H/2+size_triangle)
-    ctx.lineTo(W/2, H/2-size_triangle)
-    ctx.stroke()
+    ctxes.forEach(ctx => {
+        ctx.beginPath()
+        ctx.lineWidth = 1
+        ctx.strokeStyle = "rgb(60, 255, 0)"
+        ctx.moveTo(W/2, H/2-size_triangle)
+        ctx.lineTo(W/2-size_triangle, H/2+size_triangle)
+        ctx.lineTo(W/2+size_triangle, H/2+size_triangle)
+        ctx.lineTo(W/2, H/2-size_triangle)
+        ctx.stroke()
+
+        
+    })
 
     setInterval(() => {
         mainLoop()
@@ -24,6 +33,4 @@ window.onload = function init() {
 
 function mainLoop() {
     // ctx.clearRect(0, 0, W, H)
-
-
 }
